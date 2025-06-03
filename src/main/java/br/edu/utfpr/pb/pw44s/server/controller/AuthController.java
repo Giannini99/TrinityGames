e547850c2ad6f1f2b6b2fbbbf9e1a58abbe52434
Fabricio Giannini <gianninitrindade@gmail.com>
2025-06-03 19:50:41 -0300

    Subindo Projeto no Git

--- src/main/java/br/edu/utfpr/pb/pw44s/server/controller/AuthController.java
+++ src/main/java/br/edu/utfpr/pb/pw44s/server/controller/AuthController.java
@@ -2,19 +2,21 @@ package br.edu.utfpr.pb.pw44s.server.controller;
 
 import br.edu.utfpr.pb.pw44s.server.dto.AuthDTO;
 import br.edu.utfpr.pb.pw44s.server.dto.TokenDTO;
+import br.edu.utfpr.pb.pw44s.server.dto.UserDTO;
 import br.edu.utfpr.pb.pw44s.server.model.User;
+import br.edu.utfpr.pb.pw44s.server.security.AuthenticationService;
 import br.edu.utfpr.pb.pw44s.server.security.TokenService;
 import br.edu.utfpr.pb.pw44s.server.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/auth" )
